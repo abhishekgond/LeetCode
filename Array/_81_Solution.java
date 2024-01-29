afe878d173208fb65dd55ba365be0e8acc9fcb9d
abhishek <abhishekkumargond592@gmail.com>
2024-01-30 00:36:46 +0530

    LeetCode 33,81, problem Solution

---
+++ Array/_81_Solution.java
@@ -0,0 +1,16 @@
+public class _81_Solution {
+    // use This Method 
+    class Solution {
+        public boolean search(int[] nums, int target) {
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==target){
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+    }
+    public static void main(String args[]){
+    }
+}
